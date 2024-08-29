6425cd7b0a9af6b4c993a9f51c2d04c3c2bfd5cb
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-08-29 18:12:26 +0530

    Add files via upload

---
+++ assign.c
@@ -0,0 +1,11 @@
+#Solving previous programme using logial and
+
+age=int(input("Enter your age\n"))
+
+if age>7 and age<65:
+    print("Entry is allowed")
+    
+else:
+    print("Entry is not allowed")
+    
+        
