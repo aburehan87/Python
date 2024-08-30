49a30ea064c8ff797eeae33e4dfd252424264ab3
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-08-30 17:34:08 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,11 @@
+#A dictionary is changeable and mutable 
+capitals={'India':'New Delhi',
+          'Russia':'Moscow',
+          'China':'Beijing',
+          'USA':'Washington DC'}
+
+print(capitals['China'])
+print(capitals.get('China'))#print selected
+print(capitals.items())#to print all keys and values
+print(capitals.keys())#print all keys
+print(capitals.values())#print all values
