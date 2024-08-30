75f96f0d6f2fedb3cc09661fa021ea25570e117e
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-08-30 17:35:41 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,15 @@
+#here we create a module named example and will be using  in it our new programme by importing this example module
+
+def square(x):
+    return x**2
+
+def cube(x):
+    return print(f"The cube is :{x**3}")
+
+def area(x):
+    area=3.14*x*x
+    return print(f"The area of the circle is: {area}")
+
+def circumference(x):
+    circum=2*x*3.14
+    return print(f"The circumference is :{circum}")
