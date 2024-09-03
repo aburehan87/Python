737a6f4a73c9744e31a22a69a7f457925854b6ca
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-09-03 14:03:42 +0530

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,7 @@
+import example# here we used the example module created previously
+
+result=example.square(3) #here we called the square funcn from example module which we created earlier and accessed the module function using .method( dot method )
+print(f"The square is: {result}")
+
+result=example.area(4)
+#here also we accessed the area funcn from example module and accessed it using .method
