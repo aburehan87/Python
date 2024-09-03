03b0427f102d965863c441c68df944c9c9c85266
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-09-03 14:03:32 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,20 @@
+#module: a file containing a code which you want to include in your programme
+#for eg import time:here time is a module which contains certain code,for eg import math
+
+#you can also rename a module
+#eg:  import math as m
+
+import math as m
+
+print(m.pi)#here we replaced math with m
+
+#we can also directly call using module
+#eg: from math import pi
+
+from math import pi
+
+print(pi)
+
+
+#.method can be used to access modules
+
