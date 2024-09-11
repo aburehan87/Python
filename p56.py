83f1fe6cd22a9d4b74f208a9a5633cf508ad5586
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-09-12 00:16:07 +0530

    Add files via upload

---
+++ p56.py
@@ -0,0 +1,10 @@
+#if else statements
+#enter a number and output positive or negative
+
+x=int(input("Enter a number:\n"))
+
+if   x>0:
+    print(f"{x} is positive")
+else: 
+    print(f"{x} is negative")
+    #end
