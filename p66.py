a5b7415f6fc09353e9ac44aa69117802e02d3aa5
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-09-27 15:06:37 +0530

    Add files via upload

---
+++ p66.py
@@ -0,0 +1,5 @@
+#ternary operations using if,if elif,if else
+#enter a number and output even or odd using ternanry operators
+
+x=int(input("ENter a number\n"))
+print(f"{x} is even ") if x%2==0 else print(f"{x} is odd")
