e579fa1d69670a5545dc2c7b460d49e1c2b33fcd
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-11-09 14:56:39 +0530

    Add files via upload

---
+++ p131.py
@@ -0,0 +1,16 @@
+#using functions
+def india():
+    print("new delhi")
+    
+def japan():
+    print("tokyo")
+    
+def main():
+    print("Capital of india is :",end=" ")
+    india()
+    print("Capital of japan is:", end=" ")
+    japan()
+        
+    #end:main
+        
+main()
