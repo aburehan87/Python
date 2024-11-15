f518011cdb7373afbc4bd43e48b27d3fd0b00a2d
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2024-11-15 21:15:10 +0530

    Add files via upload

---
+++ p150.py
@@ -0,0 +1,11 @@
+square=lambda a:a*a
+cube=lambda c:c*c*c
+add=lambda e,f:e+f
+sub=lambda g,h:g-h
+def main():
+    print(square(4))
+    print(cube(3))
+    print(add(10,20))
+    print(sub(60,10))
+
+main()
